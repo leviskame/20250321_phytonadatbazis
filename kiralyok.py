3bbc5230e343087e82fca023d263fddf5393b413
Simonyi Levente <simonyi.levente@diak.szbi-pg.hu>
2025-03-21 13:33:28 +0100

    matyas kiraly adatai

--- kiralyok.py
+++ kiralyok.py
@@ -4,23 +4,32 @@ cnx = connection.MySQLConnection(user='root', password = "",
                                  host='127.0.0.1',
                                  database='kiralyok')
 
+#tablak megjelenitese
 cursor = cnx.cursor()
 cursor.execute("SHOW TABLES")
 for table in cursor: 
     print(table)
 
+print("_______________________________________________________________________")
 
+#uralkodok megjelenitese
+cursor.execute("SELECT * FROM uralkodo")
+for uralkodo in cursor: 
+    print(uralkodo)
 
+print("_______________________________________________________________________")
 
+#kivalasztas
+cursor.execute("SELECT * FROM uralkodo WHERE uralkodo.nev = 'I. Mátyás'")
+for uralkodo in cursor: 
+    print(uralkodo)
+    
+print("_______________________________________________________________________")
 
-
-
-
-
-
-
-
-
+#
+cursor.execute("SELECT szul,hal FROM uralkodo WHERE uralkodo.nev = 'I. Mátyás'")
+for uralkodo in cursor: 
+    print(f"Mátyás király élt: {uralkodo[0]} - {uralkodo[1]}")
 
 
 cnx.close()
