b89a56390030773c7488e7b88706b18706347ff6
Simonyi Levente <simonyi.levente@diak.szbi-pg.hu>
2025-03-28 13:26:05 +0100

    update

--- mydatabase.py
+++ mydatabase.py
@@ -27,3 +27,26 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS customers (name VARCHAR(255), addre
 mycursor.execute("SHOW TABLES")
 for x in mycursor:
   print(x)
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = [
+  ('Peter', 'Lowstreet 4'),
+  ('Amy', 'Apple st 652'),
+  ('Hannah', 'Mountain 21'),
+  ('Michael', 'Valley 345'),
+  ('Sandy', 'Ocean blvd 2'),
+  ('Betty', 'Green Grass 1'),
+  ('Richard', 'Sky st 331'),
+  ('Susan', 'One way 98'),
+  ('Vicky', 'Yellow Garden 2'),
+  ('Ben', 'Park Lane 38'),
+  ('William', 'Central st 954'),
+  ('Chuck', 'Main Road 989'),
+  ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+
